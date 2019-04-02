763334c7ede7413e9196c8d22d2a7d9d2e959196
Myroslava <choliymira@gmail.com>
2019-04-02 01:13:50 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+const expect = require('expect')
+const fs = require('fs')
+const jsdom = require(jsdom)
+const path = require(path)
+
+
+describe('index', ()=>{
+  
+})
+it('callas console error()',()=>{
+  
+})
+
+jsdom({
+  src:
+  fc.readFileSync(path.resolve(__dirname,'..','index.js','utf-8'))
+})
+
